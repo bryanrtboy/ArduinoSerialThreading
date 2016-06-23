e03127ca8c37d855decf608306c3484feaad1607
bryanrtboy <bryan@bryanleister.com>
2016-06-22 18:08:43 -0700

    Updated for Reading Touche
    Reads a Touche programmed capacitive touch Arduino and plots to a graph.
    Based on the Processing sketch Graph.pde included with Touche.

---
+++ Assets/Arduino/LineGraph.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LineGraph : MonoBehaviour
+{
+
+	public SerialReader m_serialReader;
+	public Transform m_node;
+	public float m_width = 100f;
+	public float yIncrement = 0.1f;
+	public int m_nodeCount = 16;
+
+	[HideInInspector]
+	public float[] ThreadSafeYValues;
+
+	Transform[] m_nodes;
+	bool isUpdating = false;
+	int yMinimum = 10000;
+	int yMaximum = 0;
+	int mean = 0;
+	float xIncrement = 0;
+
+
+	void Awake ()
+	{
+
+		float updateSpeed = m_serialReader.m_updateRate;
+		m_nodes = new Transform[m_nodeCount];
+		xIncrement = m_width / m_nodeCount;
+		for (int i = 0; i < m_nodeCount; i++) {
+			m_nodes [i] = Instantiate (m_node, transform.position, Quaternion.identity) as Transform;
+			m_nodes [i].position = new Vector3 (transform.position.x + (i * xIncrement), transform.position.y, transform.position.z);
+		}
+
+		//InvokeRepeating ("UpdatePositions", 1, m_serialReader.m_updateRate);
+	}
+
+	void Update ()
+	{
+		//UpdatePositions ();
+		if (isUpdating)
+			UpdatePositions ();
+	}
+
+	public void UpdateYPositions (float[] positions)
+	{
+		ThreadSafeYValues = positions;
+		isUpdating = true;
+	}
+
+
+	public void UpdatePositions ()
+	{
+		isUpdating = false;
+
+		if (ThreadSafeYValues == null)
+			return;
+		
+		float[] tempArray = ThreadSafeYValues;
+
+//		if (tempArray.Length > m_nodeCount)
+//			Debug.Log ("Incoming value length is " + tempArray.Length);
+
+		for (int i = 0; i < tempArray.Length; i++) {
+
+			if (i >= m_nodeCount) {
+				//Debug.Log ("Stopping...");
+				return;
+			}
+
+			int temp = (int)tempArray [i];
+
+//			if (temp <= 0)
+//				continue;
+			
+			if (temp < yMinimum && i != 0)  //offset by this amount
+				yMinimum = temp;
+			if (temp > yMaximum)
+				yMaximum = temp;
+		
+			mean = yMaximum - yMinimum;
+
+			//Debug.Log ("mean: " + mean + " yMin: " + yMinimum + " yMax: " + yMaximum + " value: " + temp + " yIncrement: " + yIncrement);
+			float f = (temp - yMinimum) * yIncrement;
+			if (f <= 10000)
+				m_nodes [i].transform.position = new Vector3 (m_nodes [i].transform.position.x, f, m_nodes [i].transform.position.z);
+
+		}
+	}
+
+}
--- Assets/Arduino/SerialReader.cs
+++ Assets/Arduino/SerialReader.cs
@@ -11,30 +11,22 @@ public class SerialReader : MonoBehaviour
 	public string m_name = "COM3";
 	public int m_baudRate = 115200;
 	public float m_updateRate = .1f;
+	public LineGraph m_lineGraph;
 
-	Thread thread;
+	Thread m_thread;
 	bool runThread = true;
-	bool updateThread;
-		
-	SerialPort sp;
+	[HideInInspector]
+	public bool updateThread;
+	[HideInInspector]
+	public bool freshData = false;
 
+	SerialPort sp;
 
 	int xValue, yValue, Command;
 	bool Error = true;
 	int ErrorCounter = 0;
 	int TotalRecieved = 0;
-	[HideInInspector]
-	public bool DataRecieved1 = false;
-	[HideInInspector]
-	public bool DataRecieved2 = false;
-	[HideInInspector]
-	public bool DataRecieved3 = false;
-	[HideInInspector]
-	public bool DataRecieved = false;
-	[HideInInspector]
-	public bool Data1Recieved = false;
-	[HideInInspector]
-	public bool Data2Recieved = false;
+	bool DataRecieved = false;
 
 	int NumOfSerialBytes = 8;
 	int[] serialInArray = new int[8];
@@ -43,42 +35,27 @@ public class SerialReader : MonoBehaviour
 	// A count of how many bytes received
 	int xMSB, xLSB, yMSB, yLSB;
 	// Bytes of data
+	[HideInInspector]
+	public List<float> incomingXValues = new List<float> (0);
+	[HideInInspector]
+	public List<float> XPacket = new List<float> (0);
+	[HideInInspector]
+	public List<float> YPacket = new List<float> (0);
+	[HideInInspector]
+	public List<float> incomingYValues = new List<float> (0);
 
-	//Dynamic arrays
-	List<float> DynamicArrayTime1 = new List<float> (0);
-	public List<float> DynamicArrayTime2 = new List<float> (0);
-	public List<float> DynamicArrayTime3 = new List<float> (0);
-	List<float> Time1 = new List<float> (0);
-	public List<float> Time2 = new List<float> (0);
-	List<float> Time3 = new List<float> (0);
-	List<float> Voltage1 = new List<float> (0);
-	List<float> Voltage2 = new List<float> (0);
-	public List<float> Voltage3 = new List<float> (0);
-	List<float> current = new List<float> (0);
-	List<float> DynamicArray1 = new List<float> (0);
-	List<float> DynamicArray2 = new List<float> (0);
-	public List<float> DynamicArray3 = new List<float> (0);
-
-	List<float> PowerArray = new List<float> (0);
-	// Dynamic arrays that will use the append()
-	List<float> DynamicArrayPower = new List<float> (0);
-	// function to add values
-	List<float> DynamicArrayTime = new List<float> (0);
 
 	void Start ()
 	{
 		foreach (string s in SerialPort.GetPortNames())
 			Debug.Log (s);
-		
+
 		sp = new SerialPort (m_name, m_baudRate);
-		sp.ReadBufferSize = 20;
 		sp.Open ();
-
-
-		thread = new Thread (ThreadUpdate); 
-		thread.Start (sp);
-
+		m_thread = new Thread (ThreadUpdate);
+		m_thread.Start (sp);
 		InvokeRepeating ("UpdateInterval", .5f, m_updateRate);
+
 	}
 
 	void OnDisable ()
@@ -90,7 +67,7 @@ public class SerialReader : MonoBehaviour
 	void UpdateInterval ()
 	{
 		updateThread = true;
-		UpdateGraph ();
+
 	}
 
 	void ThreadUpdate (object context)
@@ -99,98 +76,158 @@ public class SerialReader : MonoBehaviour
 		SerialPort serialPort = context as SerialPort;
 
 		while (runThread && serialPort.IsOpen) {
-			if (updateThread) {
+			if (updateThread) {						    //Only run the thread this often based on when it is set to true
 				updateThread = false;
 
-//				//some crazy ass function that takes forever to do here
-//				string inData = serialPort.ReadLine ();
-//				print (inData);
+				int bufferSize = 255;				    
+				byte[] buffer = new byte[bufferSize];   //make a buffer to hold a chunk of the stream
+				sp.Read (buffer, 0, bufferSize);		//Get the data
 
-				int inByte = sp.ReadByte ();
-				if (inByte == 0)
-					serialCount = 0;
-				
-				if (inByte > 255) {
-					//print (" inByte = " + inByte);
-				}
+				for (int i = 0; i < bufferSize; i++) {
 
-				// Add the latest byte from the serial port to array:
-				serialInArray [serialCount] = inByte;
-				serialCount++;
+					int inByte = buffer [i];
 
-				Error = true;
-				if (serialCount >= NumOfSerialBytes) {
-					serialCount = 0;
-
-					TotalRecieved++;
-
-					int Checksum = 0; //Error checking byte
-
-					//    Checksum = (Command + yMSB + yLSB + xMSB + xLSB + zeroByte)%255;
-					for (int x = 0; x < serialInArray.Length - 1; x++) {
-						Checksum = Checksum + serialInArray [x];
+					if (inByte == 0) {					//The Arduiono Touche program is using a 0 to signal the start of a packet
+						serialCount = 0;
 					}
 
-					Checksum = Checksum % 255;
-
-					if (Checksum == serialInArray [serialInArray.Length - 1]) {
-						Error = false;
-						DataRecieved = true;
-					} else {
-						Error = true;
-						//  println("Error:  "+ ErrorCounter +" / "+ TotalRecieved+" : "+float(ErrorCounter/TotalRecieved)*100+"%");
-						DataRecieved = false;
-						ErrorCounter++;
-						float _error = (ErrorCounter / TotalRecieved) * 100;
-						print ("Error:  " + ErrorCounter + " / " + TotalRecieved + " : " + _error + "%");
+					if (inByte > 255) {
+						print (" inByte = " + inByte);
+						return;
 					}
-				}
 
-				if (!Error) {
+					// Add the latest byte from the serial port to array:
+					serialInArray [serialCount] = inByte;
+					serialCount++;
+
+					Error = true;
+					if (serialCount >= NumOfSerialBytes) { //Once we have the 8 bites of a packet, reset
+						serialCount = 0;
+
+						TotalRecieved++;
+
+						int Checksum = 0; //Error checking byte
+
+						//    Checksum = (Command + yMSB + yLSB + xMSB + xLSB + zeroByte)%255;
+						for (int x = 0; x < serialInArray.Length - 1; x++) {
+							Checksum = Checksum + serialInArray [x];
+						}
+
+						Checksum = Checksum % 255;
+
+						if (Checksum == serialInArray [serialInArray.Length - 1]) {
+							Error = false;
+							DataRecieved = true;
+						} else {
+							Error = true;
+							//  println("Error:  "+ ErrorCounter +" / "+ TotalRecieved+" : "+float(ErrorCounter/TotalRecieved)*100+"%");
+							DataRecieved = false;
+							ErrorCounter++;
+							float _error = (ErrorCounter / TotalRecieved) * 100;
+							print ("Error:  " + ErrorCounter + " / " + TotalRecieved + " : " + _error + "%");
+						}
+					}
+
+					if (!Error) {
 
 
-					int zeroByte = serialInArray [6]; //Which values have a zero value
-					// println (zeroByte & 2);
+						int zeroByte = serialInArray [6]; //Which values have a zero value
+						// println (zeroByte & 2);
 
-					xLSB = serialInArray [3]; //X value's least significant byte
-					if ((zeroByte & 1) == 1)
-						xLSB = 0;
-					xMSB = serialInArray [2]; //X value's most significant byte     
-					if ((zeroByte & 2) == 2)
-						xMSB = 0;
+						xLSB = serialInArray [3]; //X value's least significant byte
+						if ((zeroByte & 1) == 1)
+							xLSB = 0;
+						xMSB = serialInArray [2]; //X value's most significant byte     
+						if ((zeroByte & 2) == 2)
+							xMSB = 0;
 
-					yLSB = serialInArray [5]; //Y value's least significant byte
-					if ((zeroByte & 4) == 4)
-						yLSB = 0;
+						yLSB = serialInArray [5]; //Y value's least significant byte
+						if ((zeroByte & 4) == 4)
+							yLSB = 0;
 
-					yMSB = serialInArray [4]; //Y value's most significant byte
-					if ((zeroByte & 8) == 8)
-						yMSB = 0;
+						yMSB = serialInArray [4]; //Y value's most significant byte
+						if ((zeroByte & 8) == 8)
+							yMSB = 0;
+
+						//print (serialInArray [0] + "\t Command:" + Command + "\tyMSB:" + yMSB + "\tyLSB: " + yLSB + "\ttxLSB: " + xMSB + "\txLSB: " + xLSB + "\tzeroByte " + zeroByte + "\tChecksum " + serialInArray [7]); 
+
+						// >=====< combine bytes to form large integers >==================< //
+
+						Command = serialInArray [1];
+						xValue = xMSB << 8 | xLSB;                    // Get xValue from yMSB & yLSB  
+						yValue = yMSB << 8 | yLSB;                    // Get yValue from xMSB & xLSB
+
+						//                 How that works: if xMSB = 10001001   and xLSB = 0100 0011 
+						//			       xMSB << 8 = 10001001 00000000    (shift xMSB left by 8 bits)                       
+						//			       xLSB =          01000011    
+						//			       xLSB | xMSB = 10001001 01000011    combine the 2 bytes using the logic or |
+						//			       xValue = 10001001 01000011     now xValue is a 2 byte number 0 -> 65536  
+
+//						print ("Got something: " + Command + "  " + xValue + "  " + yValue + " ");
+
+						UpdateDictionaries ();
+					}
 
 
-					//println( "0\tCommand\tyMSB\tyLSB\txMSB\txLSB\tzeroByte\tsChecksum"); 
-					//print (serialInArray [0] + "\t Command:" + Command + "\tyMSB:" + yMSB + "\tyLSB: " + yLSB + "\ttxLSB: " + xMSB + "\txLSB: " + xLSB + "\tzeroByte " + zeroByte + "\tserial in Array" + serialInArray [7]); 
+				} //end buffer read
 
-					// >=====< combine bytes to form large integers >==================< //
-
-					Command = serialInArray [1];
-
-					xValue = xMSB << 8 | xLSB;                    // Get xValue from yMSB & yLSB  
-					yValue = yMSB << 8 | yLSB;                    // Get yValue from xMSB & xLSB
-
-					//print (Command + "  " + xValue + "  " + yValue + " ");
-
-				}
-
-//                 How that works: if xMSB = 10001001   and xLSB = 0100 0011 
-//			       xMSB << 8 = 10001001 00000000    (shift xMSB left by 8 bits)                       
-//			       xLSB =          01000011    
-//			       xLSB | xMSB = 10001001 01000011    combine the 2 bytes using the logic or |
-//			       xValue = 10001001 01000011     now xValue is a 2 byte number 0 -> 65536  
-
-				Thread.Sleep (1);
+				//Thread.Sleep (1);
 			}
 		}  
+	}
+
+	void UpdateDictionaries ()
+	{
+		switch (Command) {
+		//Recieve array1 and array2 from chip, update oscilloscope
+		case 1: // Data is added to dynamic arrays
+			incomingXValues.Add (xValue);
+			incomingYValues.Add (yValue);
+			m_lineGraph.UpdateYPositions (incomingYValues.ToArray ());
+			break;
+		case 2: // An array of unknown size is about to be recieved, empty storage arrays
+			incomingXValues = new List<float> (0);
+			incomingYValues = new List<float> (0);
+			break;    
+		case 3:  // Array has finished being recieved, update arrays being drawn 
+			XPacket = incomingXValues;
+			YPacket = incomingYValues;
+//			print ("V3: " + Voltage3.Count + ", T3: " + Time3.Count + " @" + Time.time);
+			m_lineGraph.UpdateYPositions (YPacket.ToArray ());
+			DataRecieved = true;
+			break;  
+
+		//Recieve array2 and array3 from chip
+
+//		case 4: // Data is added to dynamic arrays
+//			DynamicArrayTime2.Add (xValue);
+//			DynamicArray2.Add ((yValue - 16000.0f) / 32000.0f * 20.0f);
+//			print ("V2: " + Voltage2.Count + ", Dyn T2" + Time2.Count + " @" + Time.time);
+//			break;
+//
+//		case 5: // An array of unknown size is about to be recieved, empty storage arrays
+//			DynamicArrayTime2.Clear ();
+//			DynamicArray2.Clear ();
+//			break;    
+//
+//		case 6:  // Array has finnished being recieved, update arrays being drawn 
+//			Time2 = DynamicArrayTime2;
+//			current = DynamicArray2;
+//			print ("current: " + current.Count + ", T2" + Time2.Count + " @" + Time.time);
+//			DataRecieved2 = true;
+//			break;  
+//		//         Recieve a value of calculated power consumption & add it to the 
+//		//         PowerArray.
+//		case 20:  
+//			PowerArray.Add (yValue);
+//			break; 
+//		case 21:  
+//			DynamicArrayTime.Add (xValue); 
+//			DynamicArrayPower.Add (yValue);
+//			break;
+		default:
+			break;
+		}
 	}
 
 	void OnApplicationQuit ()
@@ -206,61 +243,8 @@ public class SerialReader : MonoBehaviour
 		runThread = false;
 		//you could use thread.abort() but that has issues on iOS
 
-		while (thread.IsAlive) {
+		while (m_thread.IsAlive) {
 			//simply have main loop wait till thread ends
 		}
-	}
-
-	void UpdateGraph ()
-	{
-		switch (Command) {
-		//         Recieve array1 and array2 from chip, update oscilloscope
-		case 1: // Data is added to dynamic arrays
-			DynamicArrayTime3.Add (xValue);
-			DynamicArray3.Add (yValue);
-			break;
-		case 2: // An array of unknown size is about to be recieved, empty storage arrays
-			DynamicArrayTime3.Clear ();
-			DynamicArray3.Clear ();
-			break;    
-		case 3:  // Array has finnished being recieved, update arrays being drawn 
-			Time3 = DynamicArrayTime3;
-			Voltage3 = DynamicArray3;
-							//   println(Voltage3.length);
-			DataRecieved3 = true;
-			break;  
-		
-		//                Recieve array2 and array3 from chip
-		
-		case 4: // Data is added to dynamic arrays
-			DynamicArrayTime2.Add (xValue);
-			DynamicArray2.Add ((yValue - 16000.0f) / 32000.0f * 20.0f);
-			break;
-		
-		case 5: // An array of unknown size is about to be recieved, empty storage arrays
-			DynamicArrayTime2.Clear ();
-			DynamicArray2.Clear ();
-			break;    
-		
-		case 6:  // Array has finnished being recieved, update arrays being drawn 
-			Time2 = DynamicArrayTime2;
-			current = DynamicArray2;
-			DataRecieved2 = true;
-			break;  
-		//         Recieve a value of calculated power consumption & add it to the 
-		//         PowerArray.
-		case 20:  
-			PowerArray.Add (yValue);
-			break; 
-		case 21:  
-			DynamicArrayTime.Add (xValue); 
-			DynamicArrayPower.Add (yValue);
-			break;
-		default:
-			break;
-		}
-		
-
-
 	}
 }
