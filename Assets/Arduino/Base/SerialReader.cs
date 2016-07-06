e1565e521e2e5d9f8ac07b5c28f3eb2302a252bd
bryanrtboy <bryan@bryanleister.com>
2016-07-06 15:36:41 -0600

    Improved Serial Port reading
    Added a None field to serial port and checked for Mac/PC functionality

--- Assets/Arduino/Base/SerialReader.cs
+++ Assets/Arduino/Base/SerialReader.cs
@@ -6,6 +6,8 @@
 //do the Unity stuff and making sure to not try to do 'Unity stuff' with data that is owned by the other thread.
 //
 using UnityEngine;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Collections;
 using System.Threading;
@@ -17,6 +19,8 @@ namespace ArduinoSerialReader
 	public class SerialReader : MonoBehaviour
 	{
 		public Dropdown m_dropDownList;
+		//For Mac, my serial port name is /dev/cu.usbserial-AM01PK2W
+		//For PC, my serial port is either COM3 or COM4
 		public string m_name = "COM3";
 		public int m_baudRate = 115200;
 		public ByteReciever m_receiver;
@@ -38,16 +42,41 @@ namespace ArduinoSerialReader
 
 			if (m_dropDownList != null) {
 				m_dropDownList.ClearOptions ();
+
+				#if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
+				Debug.Log ("Found " + GetPortNames ().Length + " serial ports");
+				foreach (string s in GetPortNames()) { //Local method for getting Mac Port Names
+					m_comPortsFound.Add (s);
+				}
+				#else
 				foreach (string s in SerialPort.GetPortNames()) {
 					m_comPortsFound.Add (s);
 				}
+				#endif
+
+				m_comPortsFound.Add ("None"); //Put a default entry so we can 'change' ports
 				m_dropDownList.AddOptions (m_comPortsFound);
+
+
 
 			}
 		}
 
 		void Start ()
 		{
+			#if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
+			Debug.Log ("Found " + GetPortNames ().Length + " serial ports");
+			foreach (string s in GetPortNames()) {
+				Debug.Log (s);
+			}
+			#else
+			Debug.Log ("Found " + SerialPort.GetPortNames ().Length + " serial ports");
+			foreach (string s in SerialPort.GetPortNames()) {
+				Debug.Log (s);
+			}
+			#endif
+
+
 			if (SerialPortNameExists ())
 				OpenSerialPort ();
 			else
@@ -75,11 +104,21 @@ namespace ArduinoSerialReader
 		bool SerialPortNameExists ()
 		{
 			bool noMatches = true;
+			#if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
+			foreach (string s in GetPortNames()) {
+				if (s == m_name)
+					noMatches = false;  //At least one name matches
+				Debug.Log (s);
+			}
+
+			#else
 			foreach (string s in SerialPort.GetPortNames()) {
 				if (s == m_name)
 					noMatches = false;  //At least one name matches
 				Debug.Log (s);
 			}
+			#endif
+
 			return !noMatches; //Return true
 		}
 
@@ -142,6 +181,23 @@ namespace ArduinoSerialReader
 			while (m_thread.IsAlive) {
 				//simply have main loop wait till thread ends
 			}
+		}
+
+		string[] GetPortNames ()
+		{
+			int p = (int)Environment.OSVersion.Platform;
+			List<string> serial_ports = new List<string> ();
+         
+			// Are we on Unix?
+			if (p == 4 || p == 128 || p == 6) {
+				string[] ttys = Directory.GetFiles ("/dev/", "tty.*");
+				foreach (string dev in ttys) {
+					if (dev.StartsWith ("/dev/tty"))
+						serial_ports.Add (dev);
+					Debug.Log (String.Format (dev));
+				}
+			}
+			return serial_ports.ToArray ();
 		}
 	}
 }
